4c403a8c1961d504676011d2d8bfbcd5ae446e07
Isabela Ferreira <88989947+isabferreira@users.noreply.github.com>
2025-04-26 00:52:46 +0000

    maps

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapScreen from './MapScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapScreen/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
